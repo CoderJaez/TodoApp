053f1e9dfd77aa81c84e81a3d308d7ed0a97049f
Jaezer Akini <jaezer.razor@gmail.com>
2020-12-23 11:59:38 +0800

    Add All Files to Git

---
+++ App.js
@@ -0,0 +1,75 @@
+import { StatusBar} from 'expo-status-bar';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+// Components
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+
+export default function App() {
+const [todos, setTodos] = useState([
+  { text:'buy coffee', key:'1'},
+  {text:'create an app', key:'2'},
+  {text: 'play on the switch', key:'3'}
+]);
+
+const pressHandler =  (key) => {
+  setTodos((prevTodos) => {
+    return prevTodos.filter(todo => todo.key != key);
+  });
+}
+
+const submitHandler = (text) => {
+  if(text.length < 4) {
+    Alert.alert('OOPS!','The TODOS must be over 3 chars long',[
+      {text:'Understood', onPress : () => console.log('Alert Closed')}
+    ]);
+    return;
+  }
+  setTodos((prevTodos) => {
+    return [
+      {text: text, key: Math.random().toString()},
+      ...prevTodos
+    ]
+  })
+}
+
+
+  return (
+    <TouchableWithoutFeedback onPress = {() => {
+      Keyboard.dismiss();
+    }}>
+      <View style = {styles.container}>
+        {/* header */}
+        <Header />
+        {/* to form */}
+        <View style = {styles.content}>
+          <AddTodo submitHandler = {submitHandler}/>
+
+          <FlatList 
+            data = {todos}
+            renderItem = {({item}) => (
+              <TodoItem item = {item} pressHandler = {pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  content:{
+    flex:1,
+    padding: 40
+  },
+  list:{
+    flex: 1,
+    marginTop: 20
+  }
+});
